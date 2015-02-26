0887ab1994d03413739d9e00348bdbfaa04a9b2c
gogoTech Jeremy, Kelly, James <developers+gadget@indiegogo.com>
2015-02-25 16:35:12 -0800

    This is cray cray, but whateves

--- lib/active_model/airbrake_forbidden_attributes.rb
+++ lib/active_model/airbrake_forbidden_attributes.rb
@@ -1,21 +1,16 @@
 module ActiveModel
   module AirbrakeForbiddenAttributes
-    extend ActiveSupport::Concern
-
-    included do
-      alias_method_chain :sanitize_for_mass_assignment, :airbrake
-    end
-
-    def sanitize_for_mass_assignment_with_airbrake(*options)
-      begin
-        sanitize_for_mass_assignment_without_airbrake(*options)
-      rescue => e
+    def sanitize_for_mass_assignment(*options)
+      new_attributes = options.first
+      if new_attributes.respond_to?(:permitted?) && !new_attributes.permitted?
         Airbrake.notify_or_ignore(
-          e,
+          ActiveModel::ForbiddenAttributes.new,
           :parameters => options.try(:first),
           :cgi_data   => ENV.to_hash
         )
       end
+
+      ActiveModel::MassAssignmentSecurity.instance_method(:sanitize_for_mass_assignment).bind(self).call(*options)
     end
   end
 end
