a06d567864b8e876b3e29917ffb6459d3fd16c17
Peter Brindisi <peter@indiegogo.com>
2015-03-04 14:30:28 -0800

    raise exceptions in test env so we can fix the majority of issues

--- lib/active_model/airbrake_forbidden_attributes.rb
+++ lib/active_model/airbrake_forbidden_attributes.rb
@@ -3,6 +3,9 @@ module ActiveModel
     def sanitize_for_mass_assignment(*options)
       new_attributes = options.first
       if new_attributes.respond_to?(:permitted?) && !new_attributes.permitted?
+        # help surface these kinds of issues in the test env
+        raise(ActiveModel::ForbiddenAttributes) if Rails.env.test?
+
         Airbrake.notify_or_ignore(
           ActiveModel::ForbiddenAttributes.new,
           :parameters => options.try(:first),
